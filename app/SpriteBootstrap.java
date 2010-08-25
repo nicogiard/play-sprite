4e8de93821d9082a81c62afdf5dba83b8d0aca77
nicogiard <nicogiard@gmail.com>
2010-08-25 12:10:46 +0200

    amélioration des calculs de position et de taille

--- app/SpriteBootstrap.java
+++ app/SpriteBootstrap.java
@@ -1,6 +1,7 @@
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
@@ -41,42 +42,55 @@ public class SpriteBootstrap extends Job {
 		}
 
 		VirtualFile vf = VirtualFile.open(backgroundPath);
-		Map<VirtualFile, SpriteImage> listBImage = new HashMap<VirtualFile, SpriteImage>();
+		Map<String, SpriteImage> listBImage = new HashMap<String, SpriteImage>();
 
 		try {
 			for (VirtualFile child : vf.list()) {
-				listBImage.put(child, new SpriteImage(child.getRealFile().getAbsolutePath(), ImageIO.read(child.getRealFile())));
+				String absolutePath = child.getRealFile().getAbsolutePath();
+				listBImage.put(absolutePath, new SpriteImage(absolutePath, ImageIO.read(child.getRealFile())));
 			}
+
+			// Tri des keys en fonction de la Width des images
+			List<String> keys = new ArrayList<String>(listBImage.keySet());
+			Collections.sort(keys, new SpriteImageWidthComparator(listBImage));
 
 			// Image finale sera verticale
 			int width = 0;
 			int height = 0;
-			for (VirtualFile key : listBImage.keySet()) {
-				BufferedImage bufferedImage = listBImage.get(key).bufferedImage;
-				if (width < bufferedImage.getWidth())
-					width = bufferedImage.getWidth();
+			int x = 0;
+			int y = 0;
 
-				height += bufferedImage.getHeight();
+			if (keys != null && keys.size() > 0)
+				width = listBImage.get(keys.get(0)).bufferedImage.getWidth();
+
+			for (String key : keys) {
+				listBImage.get(key).spriteX = x;
+				listBImage.get(key).spriteY = y;
+
+				BufferedImage bufferedImage = listBImage.get(key).bufferedImage;
+				if (x + bufferedImage.getWidth() < width) {
+					x += bufferedImage.getWidth();
+					height = y + bufferedImage.getHeight();
+				} else {
+					x = 0;
+					y += bufferedImage.getHeight();
+				}
 			}
 			Logger.debug("Taille de l'image Sprite : '%sx%s'", width, height);
 
 			// Concatenation des images
 			BufferedImage dest = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-			int x = 0;
-			int y = 0;
-			for (VirtualFile key : listBImage.keySet()) {
-				listBImage.get(key).spriteX = x + "px";
-				listBImage.get(key).spriteY = y + "px";
-				BufferedImage bufferedImage = listBImage.get(key).bufferedImage;
-				Logger.debug("Ajout de l'image '%s'", key.getName());
 
-				boolean imageDrawn = dest.createGraphics().drawImage(bufferedImage, x, y, null);
+			for (String key : keys) {
+				BufferedImage bufferedImage = listBImage.get(key).bufferedImage;
+
+				Logger.debug("Ajout de l'image '%s'", listBImage.get(key));
+
+				boolean imageDrawn = dest.createGraphics().drawImage(bufferedImage, listBImage.get(key).spriteX, listBImage.get(key).spriteY, null);
 
 				if (!imageDrawn) {
-					Logger.debug("Un problème est survenu lors de l'écriture de l'image '%s'", key.getName());
+					Logger.debug("Un problème est survenu lors de l'écriture de l'image '%s'", key);
 					break;
-				} else {
-					y += bufferedImage.getHeight();
 				}
 			}
 
@@ -93,26 +107,22 @@ public class SpriteBootstrap extends Job {
 		} catch (IOException ex) {
 			ex.printStackTrace();
 		}
-
-		for (VirtualFile image : listBImage.keySet()) {
-			Logger.debug("%s", listBImage.get(image));
-		}
 	}
 
 	public class SpriteImage {
 		public String absolutePath;
-		public String spriteX;
-		public String spriteY;
+		public int spriteX;
+		public int spriteY;
 		public BufferedImage bufferedImage;
 
 		public SpriteImage(String absolutePath, BufferedImage bufferedImage) {
 			this.absolutePath = absolutePath;
-			this.spriteX = "0";
-			this.spriteY = "0";
+			this.spriteX = 0;
+			this.spriteY = 0;
 			this.bufferedImage = bufferedImage;
 		}
 
-		public SpriteImage(String absolutePath, String spriteX, String spriteY, BufferedImage bufferedImage) {
+		public SpriteImage(String absolutePath, int spriteX, int spriteY, BufferedImage bufferedImage) {
 			this.absolutePath = absolutePath;
 			this.spriteX = spriteX;
 			this.spriteY = spriteY;
@@ -121,7 +131,35 @@ public class SpriteBootstrap extends Job {
 
 		@Override
 		public String toString() {
-			return absolutePath + " : " + spriteX + " " + spriteY;
+			return absolutePath + " : " + spriteX + "px " + spriteY + "px";
+		}
+	}
+
+	public class SpriteImageWidthComparator implements Comparator<String> {
+		private Map<String, SpriteImage> images;
+
+		public SpriteImageWidthComparator(Map<String, SpriteImage> images) {
+			this.images = images;
+		}
+
+		public int compare(String id1, String id2) {
+			SpriteImage i1 = images.get(id1);
+			SpriteImage i2 = images.get(id2);
+			return i2.bufferedImage.getWidth() - i1.bufferedImage.getWidth();
+		}
+	}
+
+	public class SpriteImageHeightComparator implements Comparator<String> {
+		private Map<String, SpriteImage> images;
+
+		public SpriteImageHeightComparator(Map<String, SpriteImage> images) {
+			this.images = images;
+		}
+
+		public int compare(String id1, String id2) {
+			SpriteImage i1 = images.get(id1);
+			SpriteImage i2 = images.get(id2);
+			return i2.bufferedImage.getHeight() - i1.bufferedImage.getHeight();
 		}
 	}
 }
